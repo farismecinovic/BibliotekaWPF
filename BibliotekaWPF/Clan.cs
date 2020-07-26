003a8ff5b98cf4902de60adb0b6d6784ea93e3a1
Faris Mecinovic <mecinovicfaris@hotmail.com>
2020-07-26 14:43:42 +0100

    Add project files.

---
+++ BibliotekaWPF/Clan.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotekaWPF
+{
+    public class Clan
+    {
+        public string BrojClanske { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set;}
+        public DateTime ZadnjaClanarina { get; set; } = DateTime.Now;
+
+        public ObservableCollection<Clan> Clanovi = new ObservableCollection<Clan>();
+
+    }
+
+   
+}
---
+++ BibliotekaWPF/Knjiga.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotekaWPF
+{
+    public class Knjiga
+    {
+        public string Sifra { get; set; }
+        public string Naziv { get; set; }
+        public int BrojPrimeraka { get; set; }
+
+        public ObservableCollection<Knjiga> Knjige = new ObservableCollection<Knjiga>();
+    }
+}
